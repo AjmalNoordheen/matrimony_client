737436ccd0ee6a99b303f720812d9f46d72b6340
AjmalNoordheen <noorudheenajmal@gmail.com>
2024-02-12 09:04:14 +0530

    bug fix responciveness

--- src/Components/SideBar/SideList.jsx
+++ src/Components/SideBar/SideList.jsx
@@ -4,13 +4,15 @@ import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
 import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
 import RocketRoundedIcon from '@mui/icons-material/RocketRounded';
 import TravelExploreIcon from '@mui/icons-material/TravelExplore';
+import { useNavigate } from 'react-router-dom';
 
 function SideList() {
+    const navigate = useNavigate('')
   return (
     <>
     {/* SideBar List */}
     <div className='w-11/12 flex flex-col items-center gap-y-3  py-4 rounded-b-lg font-semibold bg-white'>
-            <div className='w-[93%] h-[3.5rem] bg-[#f4bbd7] rounded-lg flex cursor-pointer items-center gap-3'>
+            <div onClick={()=>naviget('/')} className='w-[93%] h-[3.5rem] bg-[#f4bbd7] rounded-lg flex cursor-pointer items-center gap-3'>
                 <AddHomeIcon fontSize='medium' className='ml-3 text-[#d91275]'/>
                 <h1 className='text-lg'>Home</h1>
             </div>
