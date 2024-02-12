f3bdfd8e355dd25143339b43d3a03ae86979b4f4
AjmalNoordheen <noorudheenajmal@gmail.com>
2024-02-12 09:34:35 +0530

    bug fix responciveness

--- src/Components/NavBar/Navbar.jsx
+++ src/Components/NavBar/Navbar.jsx
@@ -8,15 +8,15 @@ function Navbar() {
     <div className='w-screen h-[4.7rem] flex md:block bg-white gap-x-3 overflow-hidden'>
       
       {/* Profile */}
-      <div className='w-14 h-full flex md:hidden flex-col justify-center ml-3'>
-        <img className='w-full h-14 rounded-full' src="men.jpg" alt="" /> 
+      <div className='w-16 h-full flex md:hidden flex-col justify-center ml-3'>
+        <img className='w-full h-16 rounded-full' src="men.jpg" alt="" /> 
       </div>
 
         {/* Users Lists */}
       <div className={`w-[65%] h-full ${location.pathname==='/profile'?'hidden':''} flex md:hidden space-x-3 items-center`}>
-          {Array.from({length:4}).map((item,index)=>
-             <div key={item+index} className='w-14 h-14 rounded-full border-[#d91275] border-2'>
-             <img loading='lazy' src="pro1.webp" className='w-full h-full rounded-full' alt="" />
+          {Array.from({length:3}).map((item,index)=>
+             <div key={item+index} className='w-16 h-16  rounded-full border-[#d91275] border-2'>
+             <img loading='lazy' src="pro1.webp" className='w-full h-full rounded-full border border-yellow-600' alt="" />
            </div>
           )}
       </div>
